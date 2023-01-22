a4e6bdf81726dd1db08a65a2e2204837025a9e4a
Ukomah <henryukomah@gmail.com>
2023-01-22 20:01:41 +0000

    viewset application

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from .views import MenuView
 
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', MenuView.as_view()),
+    # path('booking/', BookingView.as_view()),
+    
+    
 ]
