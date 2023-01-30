1b54c5321a93984dd4225263a8b5b709c447dfe0
Ukomah <henryukomah@gmail.com>
2023-01-30 14:35:02 +0000

    authentication settings

--- restaurant/models.py
+++ restaurant/models.py
@@ -4,17 +4,26 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class Booking(models.Model):
-    Name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     no_of_guest = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(6)])
-    BookingDate = models.DateTimeField()
+    bookingDate = models.DateTimeField()
     
     def __str__(self):
-        return self.Name
+        return self.name
 
 class Menu(models.Model):
-    Title = models.CharField(max_length=255)
-    Price = models.DecimalField(decimal_places=2, max_digits=10)
-    Inventory = models.IntegerField()
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    inventory = models.SmallIntegerField()
 
     def __str__(self):
-        return self.Title
+        return self.title, str(self.price)
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
