004b5b32777d8f2fb23c8e728c19dc5dc4cddda1
Ukomah <henryukomah@gmail.com>
2023-01-31 21:33:21 +0000

    test functionalities

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import viewsets, routers
 from rest_framework.response import Response
-from .models import Menu,Booking
-from .serializers import MenuSerializer, BookingSerializer
+from .models import Menu,Booking, MenuItem
+from .serializers import MenuSerializer, BookingSerializer, MenuItemSerializer
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import generics
 
 # Create your views here.
 def index(request):
@@ -23,24 +24,28 @@ class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     permission_classes = [IsAuthenticated]
-        
+    authentication_classes = [TokenAuthentication]
 
 
-
-class MenuView(APIView):
+# class MenuView(APIView):
     
-    def get(self, request):
-        items = Menu.objects.all()
-        serializer = MenuSerializer(items, many=True)
-        return Response(serializer.data)
+#     def get(self, request):
+#         items = Menu.objects.all()
+#         serializer = MenuSerializer(items, many=True)
+#         return Response(serializer.data)
     
-    def post(self, request):
-        serializer = MenuSerializer(data=request.data)
+#     def post(self, request):
+#         serializer = MenuSerializer(data=request.data)
         
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"status": "success", "data": serializer.data})
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"status": "success", "data": serializer.data})
         
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
 @api_view()
 @permission_classes([IsAuthenticated])
 @authentication_classes([TokenAuthentication])
